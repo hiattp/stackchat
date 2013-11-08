4d5e6466eda1a8ddee35dcbd2eb118f19d084073
Paul Hiatt <hiattp@gmail.com>
2013-11-08 13:32:37 -0700

    added adjustments to view as extension

--- assets/js/injector.js
+++ assets/js/injector.js
@@ -1,15 +1,20 @@
-// Inject StackChat iFrame
-window.onload = function(){
+// Stack Chat iFrame Injection
+function injectStackChat(){
   var iFrame  = document.createElement("iframe");
-  iFrame.src  = "chat_panel.html"; // chrome.extension.getURL ("chat_panel.html");
+  iFrame.src  = chrome.extension.getURL("views/chat_panel.html");
   iFrame.setAttribute("frameBorder","0");
   iFrame.id = "stackchat-iframe";
-  iFrame.style.cssText = "position: fixed; top: 0px; right: 0; width: 300px;";
+  iFrame.style.cssText = "position: fixed; top: 0px; right: 0; width: 300px; z-index: 1000;";
   iFrame.style.height = window.innerHeight + "px";
-  document.body.insertBefore (iFrame, document.body.firstChild);
+  document.body.insertBefore(iFrame, document.body.firstChild);
   // Handle window resizing
   window.addEventListener('resize', resizeFrame, false);
   function resizeFrame() {
     iFrame.style.height = window.innerHeight + "px";
   }
 }
+
+var existingPanel = document.getElementById("stackchat-iframe");
+if(existingPanel){
+  document.body.removeChild(existingPanel);
+} else injectStackChat();
