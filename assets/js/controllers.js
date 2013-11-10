1f42a6b0f5ffbf37232324a1c33fdb77d8908b5b
Paul Hiatt <hiattp@gmail.com>
2013-11-09 17:22:16 -0700

    added firebase files

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,23 +1,42 @@
 var stackchat = 
-  angular.module('stackchat', []);
+  angular.module('stackchat', ['firebase']).
+  constant('firebaseRootUrl', "https://stackchat-test.firebaseIO.com").
+  factory('fbRootRef', ['firebaseRootUrl', function(fbRootUrl){
+    return new Firebase(fbRootUrl)
+  }]);
 
-stackchat.controller('ChatCtrl', ['$scope',
-    function($scope){
-      var messages = []
+stackchat.controller('ChatCtrl', ['$scope', '$window', 'fbRootRef', 'angularFireCollection',
+    function($scope, $window, fbRootRef, angularFireCollection){
+      // Setup vars
+      $scope.messages = angularFireCollection(fbRootRef.child('messages'));
       $scope.msg = "";
       $scope.anon = false;
-      for(i=0;i<20;i++) messages.push({body: "This is a long message!", username: "hiattp"})
-      messages.push({body: "this is the last message and I really appreciate the opportuntiy to do this."});
-      $scope.messages = messages;
-      var added = 0;
+      var questionId = $window.name.split("-")[2];
+      // for(i=0;i<20;i++) messages.push({body: "This is a long message!", username: "hiattp"})
+      // messages.push({body: "this is the last message and I really appreciate the opportuntiy to do this."});
+      // $scope.messages = messages;
+      
       $scope.addMessage = function(e) {
+        if(e.keyCode == 27){
+          $scope.exitStackChat();
+          return;
+        }
         if(e.keyCode && e.keyCode != 13) return;
         e.preventDefault();
         if($scope.msg == 0) return;
         var msg = {body: $scope.msg};
-        if(!$scope.anon) msg.username = added >= 3 ? "hiattp" : "tester";
-        $scope.messages.push(msg);
-        added += 1;
+        if(!$scope.anon) msg.username = "hiattp";
+        // $scope.messages.push(msg);
         $scope.msg = "";
       }
+      
+      $scope.exitStackChat = function(){
+        windowMessageBus("action:exit");
+      }
 }]);
+
+function windowMessageBus(msg){
+  window.parent.postMessage({
+    message: msg
+  }, "*");
+}
--- assets/js/injector.js
+++ assets/js/injector.js
@@ -1,10 +1,13 @@
+var questionUrlMatch = /stackoverflow\.com\/questions\/(\d+)\//i.exec(window.location.href)
+  , iFrameName = questionUrlMatch ? "stackchat-iframe-" + questionUrlMatch[1] : "stackchat-iframe";
+
 // Stack Chat iFrame Injection
 function injectStackChat(){
   var iFrame  = document.createElement("iframe");
   iFrame.src  = chrome.extension.getURL("views/chat_panel.html");
   iFrame.setAttribute("frameBorder","0");
-  iFrame.id = "stackchat-iframe";
-  iFrame.style.cssText = "position: fixed; top: 0px; right: 0; width: 300px; z-index: 1000;";
+  iFrame.id = iFrameName;
+  iFrame.style.cssText = "position: fixed; top: 0px; right: 0; width: 300px; z-index: 10000;";
   iFrame.style.height = window.innerHeight + "px";
   document.body.insertBefore(iFrame, document.body.firstChild);
   // Handle window resizing
@@ -14,7 +17,23 @@ function injectStackChat(){
   }
 }
 
-var existingPanel = document.getElementById("stackchat-iframe");
-if(existingPanel){
-  document.body.removeChild(existingPanel);
-} else injectStackChat();
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if(request.action == "open"){
+    if(!document.getElementById(iFrameName)) injectStackChat();
+  }
+  return true;
+});
+
+injectStackChat();
+
+// Message handlers
+
+addEventListener("message", function(event) {
+  if (event.origin + "/views/chat_panel.html" == chrome.extension.getURL("views/chat_panel.html")){
+    console.log("Received message from chat panel:", event);
+    if(event.data.message == "action:exit"){
+      var existingPanel = document.getElementById(iFrameName);
+      if(existingPanel) document.body.removeChild(existingPanel);
+    }
+  }
+}, false);
