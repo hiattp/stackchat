66b18a95934e650144bbeb0f300f7f275576a412
Paul Hiatt <hiattp@gmail.com>
2013-11-11 16:02:08 -0700

    added badges, auto redirect and check against inaction

--- assets/js/eventPage.js
+++ assets/js/eventPage.js
@@ -1,3 +1,29 @@
+var autoOpen = false;
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, {action: "open"}, function(){});
+  var onSO = /stackoverflow\.com\//i.exec(tab.url);
+  if(onSO) chrome.tabs.sendMessage(tab.id, {action: "open"}, function(res){
+    if(!res){ // rescue if extension was just installed and content script wasn't injected
+      autoOpen = true;
+      chrome.tabs.update(tab.id, {url: tab.url});
+    }
+  });
+  else{
+    autoOpen = true;
+    chrome.tabs.update(tab.id, {url:"http://stackoverflow.com/"});
+  }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if(request.action == "readVar:autoOpen"){
+    sendResponse({autoOpen:autoOpen});
+    autoOpen = false;
+  } else if(request.action == "setVar:autoOpen"){
+    autoOpen = true;
+  } else if(request.action == "setBadge"){
+    chrome.browserAction.setBadgeText({ text: request.displayText });
+  } else if(request.action == "clearBadge"){
+    chrome.browserAction.setBadgeText({ text: "" });
+  }
+  return true;
 });
--- assets/js/injector.js
+++ assets/js/injector.js
@@ -1,4 +1,5 @@
-var questionUrlMatch = /stackoverflow\.com\/questions\/(\d+)\//i.exec(window.location.href)
+var showBadge = true
+  , questionUrlMatch = /stackoverflow\.com\/questions\/(\d+)\//i.exec(window.location.href)
   , iFrameName = questionUrlMatch ? "stackchat-iframe-" + questionUrlMatch[1] : "stackchat-iframe";
 
 // Stack Chat iFrame Injection
@@ -19,14 +20,40 @@ function injectStackChat(){
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if(request.action == "open"){
-    if(!document.getElementById(iFrameName)) injectStackChat();
+    if(!document.getElementById(iFrameName)){
+      clearBadge();
+      injectStackChat();
+    }
+    sendResponse({success: true});
   }
   return true;
 });
 
+chrome.runtime.sendMessage({action: "readVar:autoOpen"}, function(response) {
+  if(response.autoOpen) injectStackChat();
+  else renderNumMessagesBadge();
+});
+
+// Browser Action Badge Actions
+
+function renderNumMessagesBadge(){
+  if(!questionUrlMatch) return;
+  var numMessages = 0
+    , questionMessagesRef = new Firebase("https://stackchat.firebaseIO.com/questions/"+questionUrlMatch[1]+"/message_list");
+  questionMessagesRef.on('child_added', function(ss) {
+    numMessages += 1;
+    var displayText = numMessages > 99 ? "99+" : numMessages.toString();
+    if(showBadge) chrome.runtime.sendMessage({action: "setBadge", displayText: displayText});
+  });
+}
+
+function clearBadge(){
+  showBadge = false;
+  chrome.runtime.sendMessage({action: "clearBadge"});
+}
+
 // Inter-iFrame Messaging and Events
 
-// Listen for messages from other frames
 addEventListener("message", function(event) {
   if (event.origin + "/views/chat_panel.html" == chrome.extension.getURL("views/chat_panel.html")){
     // console.log("Received message from chat panel:", event);
@@ -34,6 +61,7 @@ addEventListener("message", function(event) {
       var existingPanel = document.getElementById(iFrameName);
       if(existingPanel) document.body.removeChild(existingPanel);
     } else if(event.data.message == "action:redirect"){
+      chrome.runtime.sendMessage({action: "setVar:autoOpen"});
       window.location.href = event.data.args.newLocation;
     }
   }
